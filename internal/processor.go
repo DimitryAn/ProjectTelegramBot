ddfc8ee622b6aae1839a88b2f4aaa15ad9410bf6
Дмитрий Антонов <dmitrijantonov@MacBook-Air-Dmitrij.local>
2026-01-09 22:19:16 +0300

    delete unused files

--- internal/processor.go
+++
@@ -1,6 +0,0 @@
-package internal
-
-type Processor interface {
-	// Метод для обработки запроса пользователя и отправки ответа в чат
-	MakeResponse(text string, chatID int, userName string) error
-}
