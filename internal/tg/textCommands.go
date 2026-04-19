8599d8792048f8b40fbeb35052bb8e563efeed72
Dmitriy <144562497+DimitryAn@users.noreply.github.com>
2026-04-19 13:53:42 +0300

    add ci test  (#7)
    * add tests
    
    * add test
    
    * add ci tests
    
    ---------
    
    Co-authored-by: Dmitriy <dmitriy@DESKTOP-Q2NBA56.localdomain>

--- internal/tg/textCommands.go
+++ internal/tg/textCommands.go
@@ -1,15 +1,15 @@
 package tg
 
-const helpCommand = `I am created to remember your notes, just send the text in the message. 
+const HelpCommand = `I am created to remember your notes, just send the text in the message. 
 To view first note you have created, send /check or /check3 to view three notes
 
 ‼️‼️CAUTION‼️‼️ After viewing, the note(s) will be deleted`
 
-const startCommand = `Glad to see your 🤖! I'll help you to save your important notes.
-Let's try, please write a message!` + "\n\n" + helpCommand
+const StartCommand = `Glad to see your 🤖! I'll help you to save your important notes.
+Let's try, please write a message!` + "\n\n" + HelpCommand
 
-const deleteCommand = "Delete successfully 🗑️"
+const DeleteCommand = "Delete successfully 🗑️"
 
-const saveMessage = "Save you'r page ✅"
+const SaveMessage = "Save you'r page ✅"
 
-const emptyPageMessage = "You haven't saved any page yet 🙊"
+const EmptyPageMessage = "You haven't saved any page yet 🙊"
