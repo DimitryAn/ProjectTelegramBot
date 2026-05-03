6b7afbd5e3bcea7c1bcc31c748649b2471d446ed
Dmitriy <144562497+DimitryAn@users.noreply.github.com>
2026-05-03 22:30:34 +0300

    new test
    * add tests
    
    * add test
    
    * add ci tests
    
    * new ci workflow
    
    * add new test
    
    ---------
    
    Co-authored-by: Dmitriy <dmitriy@DESKTOP-Q2NBA56.localdomain>

--- internal/tg/processor_test.go
+++ internal/tg/processor_test.go
@@ -24,3 +24,17 @@ func TestCommandStart(t *testing.T) {
 	mockSender.AssertExpectations(t)
 
 }
+
+func TestSaveWithErr(t *testing.T) {
+	mockDb := mocks.NewMockOperation(t)
+	mockSender := mocks.NewMockSender(t)
+	mockDb.On("Save", context.Background(), "my message", "u").Return(assert.AnError)
+
+	pr := tg.NewProcessor(mockSender, mockDb, context.Background())
+
+	err := pr.MakeResponse("my message", 123, "u")
+
+	assert.Error(t, err)
+	mockDb.AssertExpectations(t)
+	mockSender.AssertExpectations(t)
+}
